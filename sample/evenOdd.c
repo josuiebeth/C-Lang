aefd7570f3267cfaacaa48cbae7ee45bad7a3847
josuiebeth <josiebethbaran9@gmail.com>
2024-05-04 00:30:58 -0700

    Add logic for even and odd

--- sample/evenOdd.c
+++ sample/evenOdd.c
@@ -4,6 +4,10 @@ int main(){
     int num = 0;
     printf("Enter Number:");
     scanf("%d", &num);
-    printf("The number is %d", num);
+    printf("The number is %d\n", num);
+    if(num%2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
     return 0;
 }
